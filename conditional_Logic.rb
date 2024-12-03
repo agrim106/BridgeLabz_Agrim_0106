3d34c4c5b5577d0259262914bd8d39a4f17de288
agrim106 <agrimchaudhary2@gmail.com>
2024-12-03 10:14:10 +0530

    Added a file on conditional statements.

---
+++ conditional_Logic.rb
@@ -0,0 +1,11 @@
+#learning about conditional Logic statements.
+something_to_be_evaluated = gets.chomp
+if something_to_be_evaluated 
+  puts "I can play games"
+else 
+  puts "clean your room."
+end
+
+if 1 < 2 
+  puts "hot diggity, 1 is less than 2"
+end
