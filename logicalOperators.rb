ad7dc5f14db6686922d4d5894da3b498e07ffe64
agrim106 <agrimchaudhary2@gmail.com>
2024-12-04 13:39:15 +0530

    Added Logical Operators learned in Ruby

---
+++ logicalOperators.rb
@@ -0,0 +1,14 @@
+puts"Learning the logicalOperators '&&' and '||' and '!=' "
+#Case1 
+a = 20
+b = 50
+c = 25
+d = 40
+#Learning the logical Operators, && 
+puts  a < b && c == a
+puts "\n"
+#Learning the operators || 
+puts a > b ||  d < a 
+puts "\n"
+#learning the operators !=
+puts a != c 
