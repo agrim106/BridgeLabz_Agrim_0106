8592f337d440fba502061438f250b9f94019994b
agrim106 <agrimchaudhary2@gmail.com>
2024-12-05 14:04:21 +0530

    Inclusion of the Array

---
+++ arrays.rb
@@ -0,0 +1,7 @@
+puts"Learning Arrays"
+my_array = [1,2,3,4,5,6]
+str_array = ["agrim","george","james","alex","marcus","peters","tom"]
+Array.new(34,false)
+
+# Ruby also has zero based indexing therefore i can select a particular element in ruby.
+puts str_array[2] 
