f16e57ff3d8034e3a6c30270b62ea3e35fc3c1bc
agrim106 <agrimchaudhary2@gmail.com>
2024-12-06 13:50:28 +0530

    Last Question on Hashes, Learned Creating a hash, learned why using symbols is a better alternative to using rocket operator.

---
+++ lastHashes.rb
@@ -0,0 +1,9 @@
+puts "Last question on Hashes."
+
+cars = {
+  american_Cars: ["Ford","Chevrolet","Dodge","Tesla"],
+  german_Cars: ["Mercedes","BMW","Volkwagen","Skoda"],
+  indian_Cars: ["Tata","Mahindra","Maruti-Suzuki"],
+  japanese_Cars: ["Nissa","Honda","Toyota"],
+}
+puts cars[:american_Cars]
