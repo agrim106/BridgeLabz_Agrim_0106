c07d5c32d022a1fb0805f1dfee6166b9309c3c2d
agrim106 <agrimchaudhary2@gmail.com>
2024-12-06 10:25:32 +0530

    Accessing Values in a Hash

---
+++ accessingValuesinaHash.rb
@@ -0,0 +1,9 @@
+seasons = {
+    "summer" => "April To August",
+    "Winter" => "November To February",
+    "Spring" => "March",
+    "Autumn" => "September and October"
+
+}
+
+puts seasons["Winter"]
