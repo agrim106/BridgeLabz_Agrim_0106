43e32423cb17621c310068f535168822418f479b
Agrim Chaudhary <agrimchaudhary2@gmail.com>
2024-12-09 14:20:21 +0530

    Delete wap.rb

--- wap.rb
+++
@@ -1,9 +0,0 @@
-#write a program if a number is even if number is odd, condition : No if-else and until loop
-for i in (1..100) do
-  case 
-  when i % 2 == 0 
-  puts " #{i} Number is even"
-  else 
-    puts "#{i} Number is odd"
-  end
-end
