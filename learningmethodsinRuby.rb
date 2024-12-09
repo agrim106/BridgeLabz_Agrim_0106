2bbc0302cbe19f4466f7595efae90552603576ad
agrim106 <agrimchaudhary2@gmail.com>
2024-12-09 14:59:35 +0530

    Learned about Parameters and Arguments, How they differ and what do they do.

---
+++ learningmethodsinRuby.rb
@@ -0,0 +1,6 @@
+puts "Learning about Parameters and Arguments in methods."
+def salut(names = "Stranger ")
+   "Salut mon ami " + names  + " " + "nous sommes mes amis, c'est chose par dans la en france! " #We dont put puts statement in methods.
+end
+puts salut("Pierre")
+puts salut 
