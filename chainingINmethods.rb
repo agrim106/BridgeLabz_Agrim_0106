8a3ab384686ff5096a3cfdc32e7ac35eac1278da
Agrim Chaudhary <agrimchaudhary2@gmail.com>
2024-12-10 10:29:58 +0530

    Delete chainingINmethods.rb

--- chainingINmethods.rb
+++
@@ -1,5 +0,0 @@
-puts "Learning Chaining in methods."
-#This is one of the greatest properties of ruby, It allows us to chain methods. 
-phrases = ["together","flock","feather,","a","of","birds"]
-
-puts phrases.reverse.join(" ").capitalize
