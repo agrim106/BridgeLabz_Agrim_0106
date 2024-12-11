6ea3b358ad1632c7e45b8f24b37ed9ba0929a5db
Agrim Chaudhary <agrimchaudhary2@gmail.com>
2024-12-11 15:16:00 +0530

    Delete exceeptionhandling.rb

--- exceeptionhandling.rb
+++
@@ -1,9 +0,0 @@
-# Learning all the keywords in Exception Handling.
-begin 
-  num = 20 / 0
-rescue ZeroDivisionError => e
-  puts "Your error is : #{e.message}"
-ensure
-  puts "Your code is now executed, With and without Exception Handling."
-end
-
