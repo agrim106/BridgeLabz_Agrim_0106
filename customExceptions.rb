12c81ad8934bc6a5e05a7ad51fdaf696a96bcfad
Agrim Chaudhary <agrimchaudhary2@gmail.com>
2024-12-11 15:15:51 +0530

    Delete customExceptions.rb

--- customExceptions.rb
+++
@@ -1,9 +0,0 @@
-class MyError < StandardError
-end
-begin 
-  raise MyError , "Something went wrong, Please try again."
-rescue MyError => e
-  puts "My errors is : #{e.message}"
-ensure
-  puts "This code has been executed with/without Exception"
-end
